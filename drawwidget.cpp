c1939b1537997a8caee3884aa2593a02a19760c9
Koalakoker <koala@koalakoker.com>
2013-10-07 21:35:46 +0200

    Fixed label and grid in DrawWidget

--- drawwidget.cpp
+++ drawwidget.cpp
@@ -46,8 +46,8 @@ DrawWidget::DrawWidget(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    m_marginX = 20;
-    m_marginY = 20;
+    m_marginX = 40;
+    m_marginY = 30;
 
     m_xmin = -10.0;
     m_xmax = 10.0;
@@ -64,21 +64,79 @@ void DrawWidget::paintEvent(QPaintEvent *)
 {
     QPainter p(this);
 
+    int i;
+
     int w = geometry().width();
     int h = geometry().height();
+
+    int fontH = p.fontMetrics().boundingRect("0").height();
+
 
     int top = m_marginY;
     int left = m_marginX;
     int bottom = h - m_marginY;
     int right = w - m_marginX;
 
-    // Draw grid
+    // Draw rect
     m_drawRect = QRect(QPoint(left,top),QPoint(right,bottom));
     p.setBrush(QBrush(QColor(255,255,255)));
     p.drawRect(m_drawRect);
 
+    // Draw vertical elements
+    int fontW;
+    int delta = m_drawRect.width() / 10;
+    float deltaLabelX = (m_xmax-m_xmin)/10;
+    float deltaLabelY = (m_ymax-m_ymin)/10;
+    for (i = 0;  i < 11; i++)
+    {
+        int x = left+(delta*i);
+        // Vertical Axe
+        if (i == 5)
+        {
+            p.setPen(QPen(Qt::SolidLine));
+        }
+        else
+        {
+            p.setPen(QPen(Qt::DotLine));
+        }
+        // Grid
+        if ((i > 0) && (i < 10))
+        {
+            p.drawLine(x,top,x,bottom);
+        }
+        // Orizontal labels
+        QString label = QString("%1").arg(m_xmin + (deltaLabelX * i));
+        fontW = p.fontMetrics().width(label);
+        p.drawText(x-(fontW/2),bottom+fontH,label);
+    }
+
+    // Draw orizzontal elements
+    delta = m_drawRect.height() / 10;
+    for (i = 0;  i < 11; i++)
+    {
+        int y = top+(delta*i);
+        // Orizontal axe
+        if (i == 5)
+        {
+            p.setPen(QPen(Qt::SolidLine));
+        }
+        else
+        {
+            p.setPen(QPen(Qt::DotLine));
+        }
+        // Grid
+        if ((i > 0) && (i < 10))
+        {
+            p.drawLine(left,y,right,y);
+        }
+        // Vertical labels
+        QString label = QString("%1").arg(m_ymax - (deltaLabelY * i));
+        fontW = p.fontMetrics().width(label);
+        p.drawText(left-fontW-5,y+(fontH/4),label);
+    }
+
     // Draw points
-    int i;
+    p.setPen(QPen(Qt::SolidLine));
     for (i = 0; i < m_points.count() - 1; i++)
     {
         FPoint la = fromGlobalToLocal(m_points.at(i));
