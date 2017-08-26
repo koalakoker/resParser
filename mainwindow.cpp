3abd99300f54d269cb36b27ee064a55b442e7e26
koalakoker <koalakoker@gmail.com>
2017-08-26 17:53:38 +0200

    Updated version to 0.2.1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,7 +8,7 @@
 #include <QDebug>
 
 #define PRJ_NAME    "resParser"
-#define PRJ_VERSION "0.2.0"
+#define PRJ_VERSION "0.2.1"
 #define PRJ_WINDOWS_TITLE (QString("%1 - v%2").arg(QString(PRJ_NAME)).arg(QString(PRJ_VERSION)))
 #define HYST_FILENAME "hyst.rp"
 #define WIKIURL "https://sourceforge.net/p/resparser/wiki/Home/"
